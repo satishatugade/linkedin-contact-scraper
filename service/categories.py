aae41ec38c970684f004d855b107a4ee2cea21e2
vaibhav-catseyesystems <vaibhav.pachpute@catseyesystems.com>
2024-12-02 11:36:22 +0530

    merged response tfidf and cosin

--- service/categories.py
+++ service/categories.py
@@ -3,11 +3,11 @@ import json
 from dotenv import load_dotenv
 import os
 import utils.logging as logger 
-# from sklearn.metrics.pairwise import cosine_similarity
-# from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
 from utils.constants import l1_tags,l2_tags,stopwords
 
-# model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def get_contributing_features(vectorizer, input_vect):
     feature_names = vectorizer.get_feature_names_out()
@@ -48,15 +48,15 @@ def grabL2Category(eventDesc, L1Category):
         X_test_vect = loaded_vectorizer.transform(data)
         feature_names = loaded_vectorizer.get_feature_names_out()
         non_zero_indices = X_test_vect.nonzero()[1]
-        contributing_words = [feature_names[idx] for idx in non_zero_indices]
-        print("Contributing words for prediction L2:", contributing_words)
+        l2_contributing_words = [feature_names[idx] for idx in non_zero_indices]
+        logger.log_message(message=f"Contributing words for prediction L2:{l2_contributing_words}",level="info" )
         predictions = loaded_model.predict(X_test_vect)
 
         L2 = predictions[0]
-        return L2  # Return the L2 prediction if the model is loaded correctly
+        return L2,l2_contributing_words
     except FileNotFoundError:
         logger.log_message(level="info",message="File Not Found Error")
-        return ""
+        return "",""
     
 def grabL1Category(eventName, eventDesc):
     load_dotenv()
@@ -78,60 +78,72 @@ def grabL1Category(eventName, eventDesc):
     
     feature_names = loaded_vectorizer.get_feature_names_out()
     non_zero_indices = X_test_vect.nonzero()[1]
-    contributing_words = [feature_names[idx] for idx in non_zero_indices]
-    logger.log_message(message=f"Contributing words for predictions {y_pred} {contributing_words}",level="info")
+    l1_contributing_words = [feature_names[idx] for idx in non_zero_indices]
+    logger.log_message(message=f"Contributing words for predictions {y_pred} {l1_contributing_words}",level="info")
     L1 = y_pred[0]
-    L2 = grabL2Category(eventDesc, L1)
+    L2,l2_contributing_words = grabL2Category(eventDesc, L1)
+    cosine_results=classify_event(combined_input)
 
     result = {
-        "L1": L1,
-        "L2": L2,
+        "L1": [L1.strip()],
+        "L2": [L2.strip()],
         "L3": ""
     }
+    if cosine_results.get("L1") is not None:
+        if str(cosine_results["L1"]).strip() not in result["L1"]:
+            result["L1"].append(cosine_results["L1"])
+            print("Not there")
+
+    if cosine_results.get("L2") is not None:
+        if str(cosine_results["L2"][0]).strip() not in result["L2"]:
+            result["L2"].append(cosine_results["L2"][0])
+    
+    result["L1"]=','.join(result["L1"])
+    result["L2"]=','.join(result["L2"])
 
     return json.dumps(result)
 
-# def precompute_embeddings(tags):
-#     return {tag: model.encode(tag) for tag in tags}
+def precompute_embeddings(tags):
+    return {tag: model.encode(tag) for tag in tags}
 
-# l1_embeddings = precompute_embeddings(l1_tags.keys())
-# l2_embeddings = precompute_embeddings(l2_tags.keys())
+l1_embeddings = precompute_embeddings(l1_tags.keys())
+l2_embeddings = precompute_embeddings(l2_tags.keys())
 
-# def classify_event(description):
-#     description_embedding = model.encode(description)
-#     # Level 1 matching: Find the best match for Level 1 tags
-#     level_1_tag = None
-#     level_1_score = 0
-#     l1_reason = ""
-#     for keyword, tag_embedding in l1_embeddings.items():
-#         score = cosine_similarity([description_embedding], [tag_embedding])[0][0]
-#         if score > level_1_score:
-#             level_1_score = score
-#             level_1_tag = l1_tags[keyword]
-#             l1_reason = keyword
-#             logger.log_message(message=f"score: {score} | tag: {level_1_tag} | keyword: {keyword}")
+def classify_event(description):
+    description_embedding = model.encode(description)
+    # Level 1 matching: Find the best match for Level 1 tags
+    level_1_tag = None
+    level_1_score = 0
+    l1_reason = ""
+    for keyword, tag_embedding in l1_embeddings.items():
+        score = cosine_similarity([description_embedding], [tag_embedding])[0][0]
+        if score > level_1_score:
+            level_1_score = score
+            level_1_tag = l1_tags[keyword]
+            l1_reason = keyword
+            logger.log_message(message=f"score: {score} | tag: {level_1_tag} | keyword: {keyword}")
 
-#     # Filter L2 tags based on the chosen L1 tag
-#     filtered_l2_tags = {
-#         keyword: tags
-#         for keyword, tags in l2_tags.items()
-#         if level_1_tag in tags
-#     }
-#     # Level 2 matching: Find the best match for Level 2 tags
-#     level_2_tag = None
-#     level_2_score = 0
-#     l2_reason = ""
-#     for keyword, tags in filtered_l2_tags.items():
-#         score = cosine_similarity([description_embedding], [l2_embeddings[keyword]])[0][0]
-#         if score > level_2_score:
-#             level_2_score = score
-#             level_2_tag = tags
-#             l2_reason = keyword
+    # Filter L2 tags based on the chosen L1 tag
+    filtered_l2_tags = {
+        keyword: tags
+        for keyword, tags in l2_tags.items()
+        if level_1_tag in tags
+    }
+    # Level 2 matching: Find the best match for Level 2 tags
+    level_2_tag = None
+    level_2_score = 0
+    l2_reason = ""
+    for keyword, tags in filtered_l2_tags.items():
+        score = cosine_similarity([description_embedding], [l2_embeddings[keyword]])[0][0]
+        if score > level_2_score:
+            level_2_score = score
+            level_2_tag = tags
+            l2_reason = keyword
 
-#     return {
-#         "level_1_tag": level_1_tag,
-#         "level_2_tag": level_2_tag,
-#         "level_1_reason": l1_reason,
-#         "level_2_reason": l2_reason
-#     }
+    return {
+        "L1": level_1_tag,
+        "L2": level_2_tag,
+        "L1_reason": l1_reason,
+        "L2_reason": l2_reason
+    }
 
